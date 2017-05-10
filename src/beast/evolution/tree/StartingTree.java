99c1c774e5079ad7eb126c7fd47ff488c69f28e4
Walter <walterxie@users.noreply.github.com>
2017-05-10 17:03:41 +1200

    #2 backup code

--- src/beast/evolution/tree/StartingTree.java
+++ src/beast/evolution/tree/StartingTree.java
@@ -10,14 +10,34 @@ public class StartingTree extends FlexibleTree {
         super(rootNode);
     }
 
-
     public FlexibleTree optimizeRoot() {
+        FlexibleTree thisTree = new FlexibleTree(this.getRoot());
+        for (int nr = 0; nr < this.getNodeCount(); nr++) {
+            Node node = getNode(nr);
+            // 2n-2
+            if (!node.isRoot()) {
+                thisTree.changeRootTo(node, 0.5);
 
 
+            }
+        }
 
-
-        return this; // or new FlexibleTree ?
+        return thisTree;
     }
 
+    public double getDistanceScore(FlexibleTree flexibleTree) {
+        double score = 0;
+
+        for (int i = 0; i < flexibleTree.getNodeCount(); i++) {
+            for (int j = i+1; j < flexibleTree.getNodeCount(); j++) {
+
+                System.out.println("i=" + i + ", j=" + j);
+
+            }
+        }
+
+
+        return score;
+    }
 
 }
